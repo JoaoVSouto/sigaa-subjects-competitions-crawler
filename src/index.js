721ed008c4e74b0d8f8b3fd4caf2e02bd5b99559
JoaoVSouto <joaovitorss99@hotmail.com>
2021-05-14 23:44:05 -0300

    :sparkles: :truck: Add whatsapp mode and regular mode

--- src/index.js
+++ src/index.js
@@ -1,48 +1,16 @@
 require('dotenv').config();
-const { Client } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
 
+const { initializeWhatsappClient } = require('./whatsappHandler');
 const { retrieveCompetitions } = require('./retrieveCompetitions');
 
-const client = new Client();
+const isWhatsAppMode = process.argv.find(arg => arg === '--mode=whatsapp');
 
-client.on('qr', qr => {
-  qrcode.generate(qr, { small: true });
-});
-
-client.on('ready', () => {
-  console.info('Client is ready!');
-});
-
-let lastRequestTimestamp = null;
-const ONE_MINUTE_IN_MS = 1000 * 60;
-
-client.on('message_create', async msg => {
-  const isFromDesiredGroup = msg.id.remote === process.env.WHATSAPP_GROUP_ID;
-  const isInvalidCommand =
-    msg.body.toLowerCase() !== 'como tá a concorrência velho?';
-
-  if (!isFromDesiredGroup || isInvalidCommand) {
-    return;
-  }
-
-  const isTooEarlyForAnotherRequest = lastRequestTimestamp
-    ? Date.now() <= lastRequestTimestamp + ONE_MINUTE_IN_MS
-    : false;
-
-  if (isTooEarlyForAnotherRequest) {
-    msg.reply('tu acabou de pedir e tu já quer dnv macho? tenha calma...');
-    return;
-  }
-
-  msg.reply('calma ae man... tô pegando aqui pra tu om...');
-
-  lastRequestTimestamp = Date.now();
-  console.info('retrieving competitions...');
-  const competitions = await retrieveCompetitions();
-  console.info('finished retrieval!');
-
-  msg.reply(competitions);
-});
-
-client.initialize();
+if (isWhatsAppMode) {
+  initializeWhatsappClient();
+} else {
+  (async () => {
+    console.info('retrieving competitions...\n');
+    const competitions = await retrieveCompetitions();
+    console.log(competitions);
+  })();
+}
